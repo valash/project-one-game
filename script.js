4beb8b85e04e27bafd3d4feb4c3c6bdfd8ead831
Virginia <virginialash1@gmail.com>
2019-02-05 16:58:25 -0500

    laid out psuedocode in javascript page

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("hello world");
+// what needs to happen 
+// page loads 
+// has one question and 2 potential answers 
+// one option is correct 
+// when click on the correct answer turn the box green alert that you are correct 
+// score goes up 100 points 
+// loads next question
+// when the wrong answer is clicked turn the box red alert that you were wrong
+// score stays the same 
+// shake next question button ??
+
+// button reloads page with new question and answers
+
+
+
