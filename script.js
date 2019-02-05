e2ea17c5525782fe8291ffb7581a562804a41ac0
Virginia <virginialash1@gmail.com>
2019-02-05 17:18:28 -0500

    laid out some more psuedo-code

--- script.js
+++ script.js
@@ -1,6 +1,45 @@
+// what i need to have 
+// need to have an array with 5 questions and 5 answers
+// need to have same 5 questions with wrong answers
+// question 1 = correct answer wrong answer 
+let question1 = [
+    correct1 = "",
+    wrong1 = ""
+]
+// question 2 = correct answer wrong answer 
+let question2 = [
+    correct2 = "",
+    wrong2 = ""
+]
+// question 3 = correct answer wrong answer 
+let question3 = [
+    correct3 = "",
+    wrong3 = ""
+]
+// question 4 = correct answer wrong answer 
+let question4 = [
+    correct4 = "",
+    wrong4 = ""
+]
+// question 5 = correct answer wrong answer 
+let question5 = [
+    correct5 = "",
+    wrong5 = ""
+]
+
 // what needs to happen 
 // page loads 
 // has one question and 2 potential answers 
+let playerScore = document.querySelector('.Score');
+let currentScore = 000;
+
+const gameSetup = function(evt) {
+    //puts question in the question box
+    //puts two answers in the option boxes
+}
+
+
+
 // one option is correct 
 // when click on the correct answer turn the box green alert that you are correct 
 // score goes up 100 points 
@@ -10,6 +49,10 @@
 // shake next question button ??
 
 // button reloads page with new question and answers
-
-
+// game 1
+// game 2 
+// game 3
+// game 4
+// game 5
+//could i potentially have these load with the different info 
 
